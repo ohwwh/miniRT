a1fdf4fa98826f6671ef5cf6df2246bf26d9f6a5
haeunko <hekoh99@naver.com>
2022-10-15 20:20:49 +0900

    parse

--- utils.c
+++ utils.c
@@ -13,30 +13,51 @@ void	free_split(char **s)
 	free(s);
 }
 
-double	ft_atod(const char *str)
-{
-	double	nb;
-	int		signe;
-	int		i;
-	int		aux;
+double int_part(const char *str, int *i) {
+	double nb;
+
+	nb = 0;
+	while (str[*i] >= '0' && str[*i] <= '9')
+    	nb = nb * 10 + (str[(*i)++] - '0');
+  	return (nb);
+}
+
+double decimal_part(const char *str) {
+	double nb;
+	int aux;
+	int i;
 
 	i = 0;
+  	nb = 0;
+  	if (str[i] == '\0')
+    	return (nb);
+  	aux = 10;
+  	if (str[i] == '.') {
+    	while (str[++i] >= '0' && str[i] <= '9') {
+    		nb = nb + (double)(str[i] - '0') / aux;
+      		aux = aux * 10;
+    	}
+  	}
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	signe = 1;
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
-			signe = -1;
-	nb = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-		nb = nb * 10 + (str[i++] - '0');
-	if (str[i] != '.')
-		return ((double)nb * signe);
-	aux = 10;
-	while (str[++i] >= '0' && str[i] <= '9')
-	{
-		nb = nb + (double)(str[i] - '0') / aux;
-		aux = aux * 10;
-	}
-	return ((double)nb * signe);
+    	i++;
+  	if (str[i] != '\0')
+    	err_handler("wrong double format");
+  	return (nb);
+}
+
+double ft_atod(const char *str) {
+	double nb;
+  	int signe;
+  	int i;
+
+  	i = 0;
+  	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+    	i++;
+  	signe = 1;
+  	if (str[i] == '-' || str[i] == '+')
+    	if (str[i++] == '-')
+      		signe = -1;
+  	nb = int_part(str, &i);
+  	nb += decimal_part(str + i);
+  	return ((double)(nb * signe));
 }
