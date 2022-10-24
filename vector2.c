41033e379319350b453d51e486c1ffced6d0d044
hekoh99 <hekoh99@naver.com>
2022-10-24 17:23:18 +0900

    vector norm

---
+++ vector2.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hako <hako@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 17:21:28 by hako              #+#    #+#             */
+/*   Updated: 2022/10/24 17:21:43 by hako             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_vec	vec_scalar_mul(t_vec vec, double s)
+{
+	vec.x *= s;
+	vec.y *= s;
+	vec.z *= s;
+	return (vec);
+}
+
+t_vec	vec_mul(t_vec vec1, t_vec vec2)
+{
+	vec1.x *= vec2.x;
+	vec1.y *= vec2.y;
+	vec1.z *= vec2.z;
+	return (vec1);
+}
+
+t_vec	vec_division(t_vec vec, double t)
+{
+	vec.x *= 1 / t;
+	vec.y *= 1 / t;
+	vec.z *= 1 / t;
+	return (vec);
+}
+
+double	vdot(t_vec vec, t_vec vec2)
+{
+	return (vec.x * vec2.x + vec.y * vec2.y + vec.z * vec2.z);
+}
+
+t_vec	vcross(t_vec vec1, t_vec vec2)
+{
+	t_vec	vec;
+
+	vec.x = vec1.y * vec2.z - vec1.z * vec2.y;
+	vec.y = vec1.z * vec2.x - vec1.x * vec2.z;
+	vec.z = vec1.x * vec2.y - vec1.y * vec2.x;
+	return (vec);
+}
